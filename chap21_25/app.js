c1e89c0f0ee15289525f0641ec02e7817c0a66eb
hinajameel <hinajameel229@gmail.com>
2020-06-21 16:43:11 +0500

    jsAssignment

---
+++ chap21_25/app.js
@@ -0,0 +1,60 @@
+///////////Q1
+// var fName= prompt("Enter your first name: ");
+// var lName= prompt("Enter your last  name: ");
+// var fullName = fName + lName;
+// document.write("Good Day "+fullName);
+
+
+///////////Q2:Write a program to take a user input about his
+// favorite mobile phone model.
+// Find and display the length of user input in your browser
+
+// var mobile = prompt("Write your favourit mobile model","Favorite mobile phone model<br>");
+// document.writeln("Your favorite mobile phone model is: "+mobile+"<br>");
+// document.writeln("The string length is: "+mobile.length )
+
+
+// //////q3 Write a program to find the index of letter “n” in the word “Pakistani”
+// //  and display the result in your browser .
+// var a = "Pakistani"
+// for( var s=0 ; s<=a.length ; s++){
+//     if (a[s]==="n"){
+//         document.write("Index of n is: "+[s]+"<br>");
+//     }
+// }
+
+// //////Q4 last index of Hello World
+// var h = "Hello World";
+// var l =h.lastIndexOf("l");
+// document.write("The last index of  l is: "+l+"<br>")
+
+// //////Q5 Letter at index-3
+// var p = "Pakistani";
+// var chat = p.charAt(3);
+// document.write("String is: "+p+"<br>");
+// document.write("Letter at index-3 is: "+chat+"<br>");
+
+
+
+// /////Q6 conct()
+// var fiName= prompt("Enter your first name: ");
+// var laName= prompt("Enter your last  name: ");
+// var fulName =fiName.concat(" ",laName);
+// document.write("Good Day "+fulName);
+
+// /////////Q7
+// var city= "Hyderabad";
+// document.write("<br>City is: "+city+"<br>");
+// var repl =city.replace("Hyder","Islam") 
+// document.write("<br>After replacement: "+repl+"<br>");
+
+
+// ////////Q8   replace all accurance of and with &
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var newmessage = message.replace(/and/g, "&");
+// document.write("<br>After replacement: "+newmessage+"<br>");
+
+
+///////Q9
+var st = "471"
+document.write(st.type);
